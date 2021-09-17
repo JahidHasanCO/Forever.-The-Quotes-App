4dd9bc2f69af36a6a215d819f40215477dad27cc
Md. Zahidul Islam <vdjsovaj@gmail.com>
2021-09-17 14:55:10 +0600

    swipe refresh

--- app/src/main/java/dev/jahidhasanco/quoteapp/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/quoteapp/MainActivity.kt
@@ -2,11 +2,14 @@ package dev.jahidhasanco.quoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import dev.jahidhasanco.quoteapp.app.QuoteApplication
 import dev.jahidhasanco.quoteapp.data.repository.Response
 import dev.jahidhasanco.quoteapp.data.viewmodels.MainViewModel
@@ -20,12 +23,16 @@ class MainActivity : AppCompatActivity() {
     lateinit var recyclerView_MainActivity: RecyclerView
     lateinit var layoutManagerV: LinearLayoutManager
     lateinit var quoteAdapter: QuoteAdapter
+    private lateinit var swipe: SwipeRefreshLayout
+    lateinit var progress_Bar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView_MainActivity = findViewById(R.id.recyclerView_MainActivity)
+        progress_Bar = findViewById(R.id.progress_Bar)
+        swipe = findViewById(R.id.swipe)
         val repository = (application as QuoteApplication).quotesRepository
         mainViewModel = ViewModelProvider(this, MainViewModelFactory(repository)).get(MainViewModel::class.java)
         quoteAdapter = QuoteAdapter(this)
@@ -37,22 +44,32 @@ class MainActivity : AppCompatActivity() {
             layoutManager = layoutManagerV
             setHasFixedSize(true)
         }
+
+        swipe.setOnRefreshListener{
+            observeQuotes()
+            swipe.isRefreshing = false
+        }
     }
 
     private fun observeQuotes() {
         mainViewModel.quotes.observe(this, Observer {
             when(it){
-                is Response.Loading -> {}
-                is Response.Success -> {
-                    it.data?.let { quote ->
-                        quoteAdapter.addQuotes(quote.results)
-//
-//                        quoteAdapter.addQuotes(quote.results)
+                is Response.Loading -> {
 
+                    progress_Bar.visibility = View.VISIBLE
+                    recyclerView_MainActivity.visibility = View.INVISIBLE
+                }
+                is Response.Success -> {
+
+                    it.data?.let { quote ->
+                        progress_Bar.visibility = View.INVISIBLE
+                        recyclerView_MainActivity.visibility = View.VISIBLE
+                        quoteAdapter.addQuotes(quote.results)
                     }
                 }
                 is Response.Error -> {
-
+                    progress_Bar.visibility = View.INVISIBLE
+                    recyclerView_MainActivity.visibility = View.INVISIBLE
                     Toast.makeText(this,"${it.errorMessage}",Toast.LENGTH_SHORT).show()
                 }
             }
